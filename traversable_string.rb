c7be8334d23aa31fa746f56efe6003fa898c7247
Jamie Rumbelow <jamie@jamierumbelow.net>
2012-02-26 16:41:48 +0000

    adding the concept of an internal char pointer

--- traversable_string.rb
+++ traversable_string.rb
@@ -1,12 +1,19 @@
 require 'minitest/autorun'
 
 module TraversableString
-  class TraversableString < String
+  def self.extended base
+    base.instance_variable_set :@char, base[0]
+    base.class.send :attr_accessor, :char
+  end
+end
+
+class TraversableStringTest < MiniTest::Unit::TestCase
+  def setup
+    @string = 'Hell yeah! I LOVE <3 GitHub.'
+    @string.send :extend, TraversableString
   end
 
-  class TraversableStringTest < MiniTest::Unit::TestCase
-    def setup
-      @string = TraversableString.new('Hell yeah! I LOVE <3 GitHub.')
-    end
+  def test_internal_char_pointer_is_at_0_initially
+    assert_equal 'H', @string.char
   end
 end
